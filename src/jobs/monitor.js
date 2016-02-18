602d9551564982dfe062697c3a67a05a26abbc62
Teagan Glenn <teagan@Teagans-MacBook-Air.local>
2016-02-18 10:29:03 -0700

    Implement monitor job

--- src/jobs/monitor.js
+++ src/jobs/monitor.js
@@ -1,1 +1,39 @@
-exports.run = function () { }
+var ds18b20 = require('../services/ds18b20.js');
+var logger = require('../services/logger.js');
+var _ = require('lodash');
+
+var config;
+
+var init = function (cfg) {
+    config = cfg || {};
+
+    logger.init(config);
+};
+
+var run = function () {
+    if (!config) {
+        console.log('MONITOR', 'No configuration present');
+        return;
+    }
+
+    config.sensors.forEach(function (sensor) {
+        if (sensor.type == 'temperature') {
+            ds18b20.read(sensor)
+                .then(function (result) {
+                    logger.log('MONITOR\tREADING\t' + result.sensor.displayName + '\t' + result.reading.F + '°F');
+
+                    if (result.sensor.allowedRange) {
+                        if (result.reading.F < result.sensor.allowedRange.low || result.reading.F > result.sensor.allowedRange.high) {
+                            logger.warn(result.sensor.displayName + ' is out of allowed range: ' + result.reading.F + '°F');
+                        }
+                    }
+                })
+                .catch(function (error) {
+                    logger.error('MONITOR\tERROR\t' + error);
+                });
+        }
+    });
+};
+
+exports.init = init;
+exports.run = run;
