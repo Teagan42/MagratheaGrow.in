e860a61cbb57269c667ead914ce82700728db7ce
Teagan Glenn <teagan@Teagans-MacBook-Air.local>
2016-02-18 10:27:30 -0700

    Logger umbrella class

---
+++ src/services/logger.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+
+var loggers = [];
+var config;
+
+var init = function (cfg) {
+    config = cfg || { };
+
+    fs.readdirSync('./src/services/loggers')
+        .forEach(function (loggerFile) {
+            var logger = require('./loggers/' + loggerFile);
+
+            if (logger.init) { logger.init(config) };
+
+            loggers.push(logger);
+        });
+};
+
+var log = function (message) {
+    loggers.forEach(function (logger) {
+        if (logger.log) { logger.log(message); }
+    });
+};
+
+var warn = function (message) {
+    loggers.forEach(function (logger) {
+        if (logger.warn) { logger.warn(message); }
+    });
+};
+
+var error = function (message) {
+    loggers.forEach(function (logger) {
+        if (logger.error) { logger.error(message); }
+    });
+}
+
+exports.init = init;
+exports.log  = log;
+exports.warn = warn;
+exports.error = error;
